9be4bf478cbbe13c19e5f8213466f6543ce3f6b7
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-11-22 15:10:36 -0300

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -2,9 +2,9 @@ class Question {
 
   constructor() {
     this.title = createElement('h1')
-    this.input1 = createInput("Enter Your Name Here....");
-    this.input2 = createInput("Enter Correct Option No..");
-    this.button = createButton('Submit');
+    this.input1 = createInput("Digite Seu Nome Aqui....");
+    this.input2 = createInput("Digite o Nº da Opção Correta...");
+    this.button = createButton('Enviar');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
@@ -25,19 +25,19 @@ class Question {
     this.title.html("MyQuiz Game");
     this.title.position(350, 0);
 
-    // this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    //this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    // this.question("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
+    // this.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
+    //this.question.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
+    // question.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
 
     this.question.position(150, 80);
-    this.option1.html("1: Everyone " );
+    this.option1.html("1: A letra E " );
     this.option1.position(150, 100);
-    this.option2.html("2: Envelope" );
+    this.option2.html("2: A letra I" );
     this.option2.position(150, 120);
-    this.option3.html("3: Estimate " );
+    this.option3.html("3: A letra M " );
     this.option3.position(150, 140);
-    this.option4.html("4: Example" );
+    this.option4.html("4: A letra O" );
     this.option4.position(150, 160);
 
     this.input1.position(150, 230);
@@ -46,16 +46,16 @@ class Question {
 
     this.button.mousePressed(()=>{
       
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
+      // this.message.html("Obrigado, Sua Resposta Foi Enviada");
       // this.message.position(350, 350);
 
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
+      // this.message.html("Obrigado, Sua Resposta Foi Enviada");
       // this.message.position();
 
-      // this.message("Thank You, Your Answer Has Been Submitted");
+      // this.message("Obrigado, Sua Resposta Foi Enviada");
       // this.message(350, 350);
 
-      // this.message.html("Thank You, Your Answer Has Been Submitted");
+      // this.message.html("Obrigado, Sua Resposta Foi Enviada");
       // this.message.position(350);
 
 
